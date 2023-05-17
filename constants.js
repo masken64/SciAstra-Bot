536fe29aaeb5100b4585892e838b047da2b26f0e
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-05-17 09:41:38 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,30 @@
+
+const prompts = [
+  ["hi", "hey", "hello", "good morning", "good afternoon"],
+  ["how are you", "how is life", "how are things"],
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+  ["who created you", "who made you"],
+  ["bye", "good bye", "goodbye", "see you later"],
+  [""],
+]
+
+const replies = [
+  ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+  [
+    "Fine... how are you?",
+    "Pretty well, how are you?",
+    "Fantastic, how are you?"
+  ],
+  ["I am just a bot made by Kartik Sharma for SciAstra"],
+  ["I am just a bot made by Kartik Sharma for SciAstra"],
+  ["Bye", "Goodbye", "See you later"]
+]
+
+const alternative = [
+  "Sorry,I don't understand"
+]
+const a = ["SciAstra helps students prepare for exams such as IAT, NEST, ISI, CMI, IACS, ICAR, CUET, and more. They offer resources and guidance to help students succeed in these competitive exams."]
+const b = ["SciAstra is located in Bhubaneswar, Odisha."]
+const c = ["SciAstra offers courses that can be purchased on their platform. These courses provide comprehensive guidance and study materials to help students prepare for competitive exams. For more information check out https://www.sciastra.com/courses/"]
+const d = ["Yes, SciAstra has both Android and iOS applications available. These applications provide features such as instant chat with mentors for students' convenience. Android - https://play.google.com/store/apps/details?id=co.tarly.voaaf&hl=en_IN&gl=US, ios - https://apps.apple.com/in/app/myinstitute/id1472483563"]
+const e = ["You can contact us at support@sciastra.com, or take a look at - https://www.sciastra.com/contact/"]
